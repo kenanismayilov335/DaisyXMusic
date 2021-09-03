466a686cff901232b0600cd4656040279c9a898c
kenanismayilov335 <74872919+kenanismayilov335@users.noreply.github.com>
2021-09-03 16:19:44 +0300

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -125,12 +125,12 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     img = Image.open("temp.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("etc/font.otf", 32)
-    draw.text((205, 550), f"Title: {title}", (51, 215, 255), font=font)
-    draw.text((205, 590), f"Duration: {duration}", (255, 255, 255), font=font)
-    draw.text((205, 630), f"Views: {views}", (255, 255, 255), font=font)
+    draw.text((205, 550), f"Ad: {title}", (51, 215, 255), font=font)
+    draw.text((205, 590), f"Vaxt: {duration}", (255, 255, 255), font=font)
+    draw.text((205, 630), f"Izleyici: {views}", (255, 255, 255), font=font)
     draw.text(
         (205, 670),
-        f"Added By: {requested_by}",
+        f"Başlatan: {requested_by}",
         (255, 255, 255),
         font=font,
     )
